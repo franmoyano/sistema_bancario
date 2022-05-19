5d794e1155d0f51b0b79ccc9db85a70165a5c512
Franco Moyano <koprammoyano@gmail.com>
2022-05-19 09:19:58 -0300

    [update] Clase Level agregada a 'constants'

---
+++ src/utn/cursojava/sistemabancario/constants/Level.java
@@ -0,0 +1,5 @@
+package utn.cursojava.sistemabancario.constants;
+
+public enum Level {
+	LOW, MEDIUM, HIGH
+}
--- src/utn/cursojava/sistemabancario/dao/Level.java
+++
@@ -1,5 +0,0 @@
-package utn.cursojava.sistemabancario.dao;
-
-public enum Level {
-	LOW, MEDIUM, HIGH
-}
